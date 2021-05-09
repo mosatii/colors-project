2a43ca99cf428e27c4821271dfda487bba51184a
Mariam <you@example.com>
2021-05-09 14:18:33 +0300

    I adjusted the code after watching 30 minutes video-please check it

--- scripts/script.js
+++ scripts/script.js
@@ -1,18 +1,42 @@
 
-function getColor() {
-    return (
-      "#" +
-      Math.random()
-        .toString(16)
-        .slice(2, 8)
-    );
-  }
-  
-  function setBackground() {
-   let bgColor = getColor();
-    // document.body.style.background = bgColor;
-    document.getElementsByClassName("generatedcolor").style.background = bgColor;
-  }
-  const btngenerate=document.getElementById("btngenerate");
-  
-  btngenerate.addEventListener("click", setBackground);
+const body = document.body;
+const pallete = document.getElementById("pallete");
+const pallete2 = document.getElementById("pallete2");
+const pallete3 = document.getElementById("pallete3");
+const pallete4 = document.getElementById("pallete4");
+
+//Random Color
+function randomColor() {
+  let randomize = Math.floor(Math.random() * 16777215).toString(16);
+  let randomize2 = Math.floor(Math.random() * 16777215).toString(16);
+  let randomize3 = Math.floor(Math.random() * 16777215).toString(16);
+  let randomize4 = Math.floor(Math.random() * 16777215).toString(16);
+  pallete.innerHTML = `#${randomize}`;
+  pallete2.innerHTML = `#${randomize}`;
+  pallete3.innerHTML = `#${randomize}`;
+  pallete4.innerHTML = `#${randomize}`;
+  body.style.background = `linear-gradient(to right, #${randomize} 0%, #${randomize} 25%, #${randomize2} 25%, #${randomize2} 50%, #${randomize3} 50%, #${randomize3} 80%,#${randomize4} 80%, #${randomize4} 100%)`;
+}
+
+// function getColor() {
+//   return (
+//     "#" +
+//     Math.random()
+//       .toString(16)
+//       .slice(2, 8)
+//   );
+// }
+
+// function setBackground() {
+//  let bgColor = getColor();
+//   // document.body.style.background = bgColor;
+//   document.getElementById("pallete").style.background = bgColor;
+//   pallete.inner
+//   document.getElementById("pallete1").style.background = bgColor;
+//   document.getElementById("pallete2").style.background = bgColor;
+//   document.getElementById("pallete3").style.background = bgColor;
+//   document.getElementById("pallete4").style.background = bgColor;
+// }
+// const btngenerate=document.getElementById("btngenerate");
+
+// btngenerate.addEventListener("click", setBackground);
